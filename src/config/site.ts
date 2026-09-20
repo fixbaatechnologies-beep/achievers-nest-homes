7c2374a45f71515fc5bd47c298949338dac365c7
Ilesanmi Emmanuel Ayodeji <fixbaa.tech@gmail.com>
2026-09-20 20:44:14 +0100

    Update site.ts

--- src/config/site.ts
+++ src/config/site.ts
@@ -2,10 +2,9 @@ export const siteConfig = {
   name: "Achievers Dorms",
   phoneDisplay: "+234 803 303 6185",
   phoneHref: "tel:+2348033036185",
-  whatsappHref: "whatsappHref:
-  "https://wa.me/2348033036185?text=Hello%20Achievers%20Dorms%2C%20I%20would%20like%20to%20book%20a%20room.%20Please%20share%20the%20available%20options%20and%20booking%20details.",",
+  whatsappHref:
+    "https://wa.me/2348033036185?text=Hello%20Achievers%20Dorms%2C%20I%20am%20interested%20in%20booking%20a%20room.%20Please%20share%20the%20available%20options%20and%20booking%20details.%20Thank%20you.",
   email: "achievershostels@gmail.com",
-  location: "Near University of Ilesa, Osun State, Nigeria",
-  // Add the client's YouTube watch or share URL when it is available.
+  location: "Near University of Ilesa",
   youtubeUrl: "https://www.youtube.com/embed/DTIJAgUOHV0",
-} as const;
+};
